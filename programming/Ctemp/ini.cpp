87580f9dca37f999fcc80d38346eff211a251902
student-ops <gogakuec@gmail.com>
2022-07-03 07:32:37 +0900

    first

---
+++ programming/Ctemp/ini.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using std::cout;
+using std::endl;
+using std::cin;
+using lint = long long;
+# define rep(i, n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+int main(){
+
+
+        return 0;
+}
